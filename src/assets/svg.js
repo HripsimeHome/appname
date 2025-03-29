2bccea79d4b573de7dae6f651050dc6397b390a3
Hripsime <m_hripsime@mail.ru>
2025-03-29 18:33:12 +0400

    Added profile image

--- src/assets/svg.js
+++ src/assets/svg.js
@@ -1,14 +1,4 @@
-export const arrowPrevIcon = "arrowPrev";
-
-// Footer
-export const futuresIcon = "futures";
-export const positionIcon = "position";
-export const crypto500Icon = "crypto500";
-export const usdtIcon = "usdt";
-export const moneyIcon = "money";
-export const stackbitIcon = "stackbit"
+export const cameraIcon = "camera";
 
 
-// 1 - home=futures , 2 -profile=usdt, 3 - crypto=crypto, 4 -orders=position
-
-// 1 - home=futures , 2= profile=position, 3 - crypto=crypto, 4 -profile=usdt, 
+ 
