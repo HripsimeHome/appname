e246c2f94b02b5105eda532dc2948a6addcacac1
Hripsime <m_hripsime@mail.ru>
2025-03-29 20:27:09 +0400

    Added webp images

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,5 +1,11 @@
 import profileImage from "./images/registration/profile.jpg";
+//import homeBgImage from "./images/home/homeBg.png";
+//import homeBgWebpImage from "./images/home/homeBg.webp";
+ 
 
 export {  
+ // homeBgImage,
+ // homeBgWebpImage,
+
   profileImage,
 };
