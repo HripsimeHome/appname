51aebec7a6dbf2003877ec700c33d0efa0296de1
Hripsime <m_hripsime@mail.ru>
2025-04-03 14:42:08 +0400

    Added map for card

--- src/components/global/Card/Card.jsx
+++ src/components/global/Card/Card.jsx
@@ -48,88 +48,116 @@ import {
 
 } from "../../../assets/images"
 
+const cardData = [
+  {
+    category: "Видеоигры",
+    profilePhoto: profileImage,
+    username: "User 1",
+    image: videogameImage,
+    webpImage: videogameWebpImage,
+    title: "Lorem ipsum dolor sit amet consectetur adip",
+    date: "21.02.25",
+    time: "12:30",
+    location: "Санкт-Питербург",
+    age: "18+",
+    genderSpecific: "Для девушек",
+    forKids: "Для детей",
+    petFriendly: "С животными",
+   // className: "",
+  },
+  {
+    category: "Встречи",
+    profilePhoto: profileImage,
+    username: "User 2",
+    image: meetupImage,
+    webpImage: meetupWebpImage,
+    title: "Sit amet consectetur adip",
+    date: "05.03.25",
+    time: "10:00",
+    location: "Москва",
+    age: "18+",
+    genderSpecific: "Для всех",
+    forKids: "Для детей",
+    petFriendly: "С животными",
+     // className: "",
+  },
+  
+];
+
 
 const Card = () => {
   return (   
     <>
-      <Link to={singlePagePath}>
+    {cardData.map(
+      ({ category, profilePhoto, username, image, webpImage, title, date, time, location, age, genderSpecific, forKids, petFriendly }, index) => (
+      <Link to={singlePagePath} key={index}>
         <div className={styles.card}>      
           <div className={styles.card__header}>
             <div className={styles.card__profileContainer}>      
               <img
-                src={profileImage}          
+                src={profilePhoto}
                 alt="Profile photo"            
                 className={styles.card__profilePhoto}
               />     
-            <div className={styles.card__username}>Имя</div>
+            <div className={styles.card__username}>{username}</div>
           </div>
 
           <ImageWebp
-            src={videogameImage}
-            srcSet={videogameWebpImage}
-            //alt={alt}       
-            // className={styles.categoriesMain__categoryImg}
-          />
-        </div>
-
-
-
-
+             src={image}
+             srcSet={webpImage}
+              //alt={alt}       
+              // className={styles.categoriesMain__categoryImg}
+            />
+          </div>
 
           <h4 className={styles.card__title}>
-            Lorem ipsum dolor sit amet consectetur adip        
+            {title}        
           </h4>
-
 
           <div className={styles.card__infoContainerBig}>
             <div className={styles.card__greyPanel}>
               <div className={styles.card__iconPosition}>        
-                <label className={styles.card__clockIcon}></label>
-                <span>21.02.25,</span>
-                <span>12:30</span>
+                <label className={styles.card__clockIcon}></label>                
+                <span>{date},</span>
+                <span>{time}</span>
               </div>
             </div>
 
-
             <span className={styles.card__greyPanel}>
-              <strong>Видеоигры</strong>
+              <strong>{category}</strong>
             </span>
 
             <div className={styles.card__greyPanel}>      
               <div className={styles.card__iconPosition}>        
                 <label className={styles.card__locationIcon}></label>
-                <span>Санкт-Питербург</span>          
+                <span>{location}</span>          
               </div>        
-            </div>
-          
+            </div>          
           </div>  
 
-
           <hr className={styles.card__greyLine} />
-
           <div className={styles.card__infoContaineSmall}>
-
             <span className={styles.card__greyPanel}>
-            18+
+            {age}
             </span>
 
             <span className={styles.card__greyPanel}>
-            Для девушек
+            {genderSpecific}
             </span>
 
             <span className={styles.card__greyPanel}>
-            Для детей
+            {forKids}
             </span>
 
             <span className={styles.card__greyPanel}>
-            С животными
+            {petFriendly}
             </span>
           </div>
 
-
-
         </div>   
       </Link>
+      )
+    )}
     </>    
   );
 };
